36dc5a87fa59d1f6b9e99ffb1805456012ce9b38
Ray Hsu <ray199185@gmail.com>
2021-04-03 17:46:57 +0800

    finished

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -18,6 +18,26 @@ class CandidatesController < ApplicationController
     end
   end
 
+  def edit
+    @candidate = Candidate.find_by(id: params[:id])
+  end
+
+  def update
+    @candidate = Candidate.find_by(id: params[:id])
+
+    if @candidate.update(candidate_params)
+      redirect_to candidates_path, notice: "更新資料成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @candidate = Candidate.find_by(id: params[:id])
+    @candidate.destroy if @candidate
+    redirect_to candidates_path, notice: "資料刪除成功"
+  end
+
   private
   def candidate_params
     params.require(:candidate).permit(:name, :age, :party, :politics)
