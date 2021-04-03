f4ddf6eb03ba208a4cfe7818cb4b3d402d87e43d
Ray Hsu <ray199185@gmail.com>
2021-04-03 16:30:48 +0800

    add create

---
+++ app/controllers/candidates_controller.rb
@@ -0,0 +1,25 @@
+class CandidatesController < ApplicationController
+  
+  def index
+    @candidates = Candidate.all
+  end
+
+  def new 
+    @candidate = Candidate.new
+  end
+
+  def create
+    @candidate = Candidate.new(candidate_params)
+
+    if @candidate.save
+      redirect_to candidates_path, notice: "新增候選人成功"
+    else
+      render :new
+    end
+  end
+
+  private
+  def candidate_params
+    params.require(:candidate).permit(:name, :age, :party, :politics)
+  end
+end
